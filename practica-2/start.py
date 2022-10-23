dc757873854da9cf2296a8b0ae3d5f80aae2ab31
sensito <danielivanhola@gmail.com>
2022-10-23 16:20:24 -0500

    ruta generica

--- practica-2/start.py
+++ practica-2/start.py
@@ -1,6 +1,12 @@
 #iniciar el servidor con el comando uvicorn start:app --port 8125 --reload
 
+
+
 import os
 
+import uvicorn
 if __name__ == "__main__":
-    os.system("uvicorn main:app --port 8125 --reload") 
+    # ruta donde esta este archivo ejecutandoce actualmente
+    path = os.path.dirname(os.path.abspath(__file__))
+    # iniciar el servidor ruta del archivo donde esta la clase FastAPI
+    os.system(f"uvicorn main:app --port 8125 --app-dir {path}")
